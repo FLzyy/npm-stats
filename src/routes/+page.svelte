ce95058de20efbf5317c696d3caaf3e0cac2bcd8
FLzyy <flzyy72@gmail.com>
2023-01-10 20:38:18 +0800

    basic static stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Welcome to npm-stats</h1>
+
+	<form method="POST">
+		<label
+			>NPM Package Name: <input
+				aria-label="NPM Package Name here"
+				type="text"
+				id="pkgname"
+				name="Package Name"
+			/></label
+		>
+		<button>Go!</button>
+	</form>
+</main>
+
+<style>
+	h1 {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-weight: 600;
+	}
+
+	input,
+	button {
+		color: black;
+	}
+
+	form {
+		text-align: center;
+	}
+
+	label {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-weight: 500;
+	}
+</style>
